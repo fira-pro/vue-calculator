72fcd5b9f1d2e6850228b53aed6c3a32aae4d4e8
fira-pro <85739903+fira-pro@users.noreply.github.com>
2022-12-09 23:23:51 +0300

    basic ui and with v-for rendering

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+import Calculator from './components/Calculator.vue'
+</script>
+
+<template>
+  <div>
+    <Calculator/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,76 @@
+<script setup>
+
+const basicCalRows = {
+    row1: {
+        0: { value: 0, id: "num-0", class: "btn num" },
+        decpoint: { value: '.', id: 'decimal-point', class: 'btn' },
+        equal: { value: '=', id: 'equality', class: 'btn ' },
+        add: { value: '+', id: 'addition', class: 'btn op' },
+    },
+    row2: {
+        1: { value: 1, id: "num-1", class: "btn num" },
+        2: { value: 2, id: "num-2", class: "btn num" },
+        3: { value: 3, id: "num-3", class: "btn num" },
+        sub: { value: '-', id: 'subtraction', class: 'btn op' },
+    },
+    row3: {
+        4: { value: 4, id: "num-4", class: "btn num" },
+        5: { value: 5, id: "num-5", class: "btn num" },
+        6: { value: 6, id: "num-6", class: "btn num" },
+        mult: { value: '×', id: 'multiplication', class: 'btn op' },
+    },
+    row4: {
+        7: { value: 7, id: "num-7", class: "btn num" },
+        8: { value: 8, id: "num-8", class: "btn num" },
+        9: { value: 9, id: "num-9", class: "btn num" },
+        div: { value: '÷', id: 'division', class: 'btn op' },
+    },
+    row5: {
+        openbrkt: { value: '(', id: 'open-bracket', class: 'btn' },
+        closebrkt: { value: ')', id: 'open-bracket', class: 'btn' },
+        perc: { value: '%', id: 'percentage', class: 'btn op' },
+        allClear: { value: 'AC', id: 'all-clear', class: 'btn' }
+    }
+}
+
+const revBasicCalRows = Object.keys(basicCalRows).reverse();
+
+</script>
+
+<template>
+    <div id="calc-container">
+        <div id="calc-btns">
+            <table>
+                <tr v-for="row in Object.keys(basicCalRows).reverse()" :key="row">
+                    <td v-for="btn in basicCalRows[row]" :key="btn.id" :class="btn.class" :id="btn.id">
+                        {{ btn.value }}
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div id="calc-screen">
+        </div>
+
+    </div>
+</template>
+
+
+<style scoped>
+#calc-btns {
+margin: auto;
+}
+
+#calc-btns table {
+    border-spacing: 10px;
+}
+
+td {
+    border: 2px solid black;
+    width: 23%;
+    text-align: center;
+    font-size: 1.5em;
+    background: gray;
+    user-select: none;
+
+}
+</style>
